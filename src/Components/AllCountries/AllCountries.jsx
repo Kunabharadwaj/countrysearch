835987733e0ce48928eb479c6344ebc79b6a0284
Bharadwaj <kunabharadwaj520@gmail.com>
2022-11-26 21:58:31 +0000

    managed to view all country cards on the client

--- src/Components/AllCountries/AllCountries.jsx
+++ src/Components/AllCountries/AllCountries.jsx
@@ -32,7 +32,29 @@ function AllCountries() {
   return (
     <div className="all_country_wrapper">
       <div className="country_top"></div>
-      <div className="country_bottom"></div>
+      <div className="country_bottom">
+        {isLoading && !error && <h4>Loading....</h4>}
+        {error && !isLoading && <h4>{error}</h4>}
+
+        {
+            countries?.map(country=>(
+                
+                <div className="country_card">
+                    <div className="country_img">
+                        <img src={country.flags.png} alt=''/>
+                    </div>
+                    <div className="country_data">
+                        <h3>{country.name.common}</h3>
+                        <h6>Population:{country.population}</h6>
+                        <h6>Region:{country.Region}</h6>
+                        <h6>Capital:{country.capital}</h6>
+
+                    </div>
+                </div>
+                
+            ))
+        }
+      </div>
     </div>
   );
 }
